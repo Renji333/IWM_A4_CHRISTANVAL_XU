8fbb7b0a9508b2b7106fd2a1e82a4ca2f19b012c
FrancisENCSIO2 <francis@MacBook-Pro-de-FRANCIS.local>
2017-10-11 16:50:43 +0200

    SideBar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,3 +1,9 @@
 <div class="col-lg-3 Sidebar">
+    @foreach ($mangas as $manga)
 
+        <div>
+            <a href="/manga/{{ $manga }}">{{ $manga }}</a>
+        </div>
+
+    @endforeach
 </div>
