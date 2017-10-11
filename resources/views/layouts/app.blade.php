a26768fbf73cb569f90c0a289b3228090b7b88f5
FrancisENCSIO2 <francis@MacBook-Pro-de-FRANCIS.local>
2017-10-11 10:31:49 +0200

    Modification du app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-
+    @include('layouts.app.header')
     <body>
-
+        @yield('content)
+        @include('layouts.app.footer')
     </body>
 </html>
