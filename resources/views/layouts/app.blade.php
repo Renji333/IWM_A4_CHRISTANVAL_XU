073e4dd868aa0091b783f438c2e27fa320092332
BiosKi1 <bioski10@gmail.com>
2017-10-14 11:21:51 +0200

    bg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
     @include('layouts.partials.header')
-    <div class="container-fluid">
+    <div class="container-fluid bg">
         @yield('content')
         @include('layouts.partials.sidebar')
     </div>
