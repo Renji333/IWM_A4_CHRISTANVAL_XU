35926b0cb39e8fcbd59ca2f8ba8b9c8faa6f694a
FrancisENCSIO2 <francis@MacBook-Pro-de-FRANCIS.local>
2017-10-14 11:02:04 +0200

    Home Modif

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,92 +3,18 @@
 @section('content')
         <div class="col-lg-9 MainContent">
             <div class="row">
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/bleach.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Bleach</h3>
-                            </div>
-                        </a>
+                @foreach ($mangasC as $manga)
+                    <div class="col-lg-4" style="text-align: center">
+                        <div class="thumbnail">
+                            <a href="mangas/{{$manga->id}}">
+                                <img src="{{asset('mangas/cover/'.$manga->shortTitle.'.png')}}" alt="">
+                                <div class="caption">
+                                    <h3>{{$manga->title}}</h3>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/boruto.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Boruto</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/dbs.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Dragon ball super</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/gantz.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Gantz</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/gintama.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Gintama</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/hxh.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Hunter X Hunter</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-6" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/naruto.png')}}" alt="">
-                            <div class="caption">
-                                <h3>Naruto</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-6" style="text-align: center">
-                    <div class="thumbnail">
-                        <a href="">
-                            <img src="{{asset('mangas/cover/onepiece.png')}}" alt="">
-                            <div class="caption">
-                                <h3>One piece</h3>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
 @endsection
