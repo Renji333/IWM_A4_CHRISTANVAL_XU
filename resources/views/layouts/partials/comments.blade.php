268727ae0ca048fefa4882393a0bb6b6ced698e4
FrancisENCSIO2 <francis@MacBook-Pro-de-FRANCIS.local>
2017-10-15 13:27:41 +0200

    Fin BG

--- resources/views/layouts/partials/comments.blade.php
+++ resources/views/layouts/partials/comments.blade.php
@@ -1,5 +1,5 @@
-<div class="row">
-    <div class="col-lg-12">
+<div class="row ">
+    <div class="col-lg-12 BgWhite">
         <div class="page-header">
             <h1>Commentaires</h1>
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,7 +4,7 @@
     <div class="col-lg-9 MainContent">
         <div class="row">
             <div class="col-lg-12">
-                <img src="{{asset('mangas/'.$manga->shortTitle.'/'.$manga->tomeCurrent->chapter.'/'.$manga->pageCurrentDisplayed.'.jpg')}}"/>
+                <img class="imgMain" src="{{asset('mangas/'.$manga->shortTitle.'/'.$manga->tomeCurrent->chapter.'/'.$manga->pageCurrentDisplayed.'.jpg')}}"/>
             </div>
         </div>
         @include('layouts.partials.comments')
